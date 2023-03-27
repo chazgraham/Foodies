d8af62e1f5fb221548d803ab1118c0adf96c6021
chaz Graham <chazhg80.cg@gmail.com>
2023-03-26 20:33:58 -0500

    added api call for recipes with basic information map and styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import { Jumbotron, Container, Col, Form, Button } from 'react-bootstrap';
+import { Jumbotron, Container, Col, Form, Button, Card, ListGroup, CardGroup } from 'react-bootstrap';
 
 const SearchBar = () => {
+    const [SearchedRecipe, setSearchRecipe] = useState([]);
     const [searchedInput, setSearchInput] = useState('');
 
     const handleFormSubmit = async (event) => {
@@ -12,17 +13,32 @@ const SearchBar = () => {
         }
 
         try {
-            const response = searchedInput
+            const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=f45007eefd874c71bd0a103aa764db2d&query=${searchedInput}&number=5&addRecipeInformation=true`)
+            console.log(response)
 
             if (!response.ok) {
                 throw new Error('something went wrong!');
             }
 
+            const recipes = await response.json();
+            console.log(recipes);
+            const recipe = recipes.results;
+
+            const recipeData = recipe.map((recipe) => ({
+                title: recipe.title,
+                image: recipe.image,
+                summary: recipe.summary,
+                dishTypes: recipe.dishTypes,
+                preparationMinutes: recipe.preparationMinutes,
+                servings: recipe.servings
+            }))
+
+            setSearchRecipe(recipeData)
+            setSearchInput('');
+
         } catch (err) {
             console.error(err);
         }
-
-        setSearchInput('');
     }
 
     return (
@@ -50,6 +66,30 @@ const SearchBar = () => {
                     </Form>
                 </Container>
             </Jumbotron>
+
+            <Container>
+                <h2 className='viewing'>
+                    {SearchedRecipe.length
+                        ? `Viewing ${SearchedRecipe.length} results:`
+                        : ''}
+                </h2>
+                <CardGroup>
+                    {SearchedRecipe.map((recipe) => (
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={recipe.image} alt={`${recipe.title}`} />
+                            <Card.Body>
+                                <Card.Title>{recipe.title}</Card.Title>
+                                <Card.Text></Card.Text>
+                            </Card.Body>
+                            <ListGroup className="list-group-flush">
+                                <ListGroup.Item>Dish type- {recipe.dishTypes}</ListGroup.Item>
+                                <ListGroup.Item>preperation Time- {recipe.preparationMinutes} minutes</ListGroup.Item>
+                                <ListGroup.Item>servings- {recipe.servings}</ListGroup.Item>
+                            </ListGroup>
+                        </Card>
+                    ))}
+                </CardGroup>
+            </Container>
         </>
     )
 }
